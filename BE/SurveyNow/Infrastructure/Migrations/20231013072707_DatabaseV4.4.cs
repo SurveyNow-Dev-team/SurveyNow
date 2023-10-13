d785aa773dfe921442c2b60b026227407a7437b2
Blue Teeth <daominhtri1000@gmail.com>
2023-10-13 14:35:09 +0700

    update db

---
+++ BE/SurveyNow/Infrastructure/Migrations/20231013072707_DatabaseV4.4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseV44 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalParticipant",
+                table: "Surveys",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalParticipant",
+                table: "Surveys");
+        }
+    }
+}
